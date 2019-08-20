054089702e04d89d39a80ebdc4549d3a8908d750
titrxw <1120309488@qq.com>
2019-08-20 17:20:26 +0800

    添加生成process的命令

---
+++ src/Command/Make/ProcessCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Rangine Dev Tool
+ *
+ * (c) We7Team 2019 <https://www.rangine.com>
+ *
+ * document http://s.w7.cc/index.php?c=wiki&do=view&id=317&list=2284
+ *
+ * visited https://www.rangine.com for more details
+ */
+
+namespace W7\DevTool\Command\Make;
+
+use W7\Console\Command\GeneratorCommandAbstract;
+
+class ProcessCommand extends GeneratorCommandAbstract {
+	protected $description = 'generate process';
+
+	protected function before() {
+		$this->name = ucfirst($this->name) . 'Process';
+	}
+
+	protected function getStub() {
+		return dirname(__DIR__, 1) . '/Stubs/Process.stub';
+	}
+
+	protected function replaceStub() {
+		$stubFile = $this->name . '.stub';
+		$this->replace('{{ DummyNamespace }}', 'W7\App\Process', $stubFile);
+		$this->replace('{{ DummyClass }}', $this->name, $stubFile);
+	}
+
+	protected function savePath() {
+		return 'app/Process';
+	}
+}
