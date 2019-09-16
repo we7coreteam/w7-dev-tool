65d712d728cd3dd478797af2c98fa63e1de96ea7
titrxw <1120309488@qq.com>
2019-09-16 16:41:38 +0800

    修改生成command命令参数格式限制

--- src/Command/Make/CommandCommand.php
+++ src/Command/Make/CommandCommand.php
@@ -13,12 +13,25 @@
 namespace W7\Command\Command\Make;
 
 use W7\Console\Command\GeneratorCommandAbstract;
+use W7\Core\Exception\CommandException;
 
 class CommandCommand extends GeneratorCommandAbstract {
 	protected $description = 'generate command';
+	private $path;
 
 	protected function before() {
-		$this->name = ucfirst($this->name) . 'Command';
+		if (strpos($this->name, '/') === false) {
+			throw new CommandException('option name error,  the correct format is namespace/name');
+		}
+		$path = explode('/', $this->name);
+		foreach ($path as &$item) {
+			$item = ucfirst($item);
+		}
+		$path[count($path) - 1] .= 'Command';
+
+		$this->name = end($path);
+		array_pop($path);
+		$this->path = implode('/', $path);
 	}
 
 	protected function getStub() {
@@ -27,11 +40,11 @@ class CommandCommand extends GeneratorCommandAbstract {
 
 	protected function replaceStub() {
 		$stubFile = $this->name . '.stub';
-		$this->replace('{{ DummyNamespace }}', 'W7\App\Command', $stubFile);
+		$this->replace('{{ DummyNamespace }}', 'W7\App\Command\\' . str_replace('/', '\\', $this->path), $stubFile);
 		$this->replace('{{ DummyClass }}', $this->name, $stubFile);
 	}
 
 	protected function savePath() {
-		return 'app/Command';
+		return 'app/Command/' . $this->path;
 	}
 }
