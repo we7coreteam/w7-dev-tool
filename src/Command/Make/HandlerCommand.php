0af6eb9226ee0cf391d9df22b454d292c600683a
titrxw <1120309488@qq.com>
2020-01-10 19:22:15 +0800

    合并make:handler 和make:exceptionhandler

--- src/Command/Make/HandlerCommand.php
+++ src/Command/Make/HandlerCommand.php
@@ -19,10 +19,27 @@ use W7\Core\Exception\CommandException;
 class HandlerCommand extends GeneratorCommandAbstract {
 	protected $description = 'generate handler';
 	protected $type;
+	protected $supportType = ['session', 'log', 'cache', 'view', 'exception'];
+	protected $ignoreNameOfType = ['exception'];
 
 	protected function configure() {
 		$this->addOption('--type', null, InputOption::VALUE_REQUIRED, 'handler type');
 		parent::configure();
+	}
+
+	protected function handle($options) {
+		if (empty($this->input->getOption('type'))) {
+			throw new CommandException("option type Can't be empty");
+		}
+		$this->type = $this->input->getOption('type');
+		if (!in_array($this->type, $this->supportType)) {
+			throw new CommandException('not support the type');
+		}
+		if (in_array($this->type, $this->ignoreNameOfType)) {
+			$options['name'] = $this->type;
+		}
+
+		parent::handle($options);
 	}
 
 	protected function before() {
