37dfeb702db075e5fb4e54b9edee500476e54559
renchao <donknap@gmail.com>
2020-02-11 15:51:26 +0800

    优化make:model命令，增加--table --connection参数，可以生成完整的Orm对象

--- src/Command/Make/ModelCommand.php
+++ src/Command/Make/ModelCommand.php
@@ -12,35 +12,139 @@
 
 namespace W7\Command\Command\Make;
 
-use W7\Console\Command\GeneratorCommandAbstract;
+
+use http\Exception\RuntimeException;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Input\InputOption;
+use W7\Core\Exception\CommandException;
 
 class ModelCommand extends GeneratorCommandAbstract {
 	protected $description = 'generate model';
-	private $path;
+	protected $typeSuffix = '';
 
-	protected function before() {
-		$path = explode('/', $this->name);
-		foreach ($path as &$item) {
-			$item = ucfirst($item);
+	private $column = [];
+	private $table = '';
+	private $connection = 'default';
+
+	protected function configure() {
+		$this->addOption('--table', null, InputOption::VALUE_REQUIRED, 'table name');
+		$this->addOption('--connection', null, InputOption::VALUE_OPTIONAL, 'table connection');
+		parent::configure();
+	}
+
+	protected function handle($options) {
+		$this->table = $this->input->getOption('table');
+		$this->connection = $this->input->getOption('connection') ?: 'default';
+
+		if (!empty($this->table)) {
+			try {
+				$this->column = $this->getTableColumn($this->table, $this->connection);
+			} catch (\Exception $e) {
+				throw new CommandException($e->getMessage());
+			}
+		} else {
+			$this->column = [
+				'primaryKey' => '',
+				'field' => [],
+			];
 		}
 
-		$this->name = end($path);
-		array_pop($path);
-		$this->path = implode('/', $path);
+		if (empty($options['name']) && !empty($this->table)) {
+			$options['name'] = $this->parseTableNameToClassName($this->table);
+		}
+		parent::handle($options);
+	}
+
+	protected function replaceStub() {
+		parent::replaceStub();
+
+		$stubFile = $this->name['class'] . '.stub';
+
+		$this->replace('{{ DummyTableName }}', $this->table, $stubFile);
+		$this->replace('{{ DummyConnection }}', $this->connection, $stubFile);
+		$this->replace('{{ DummyTableKey }}', $this->column['primaryKey'], $stubFile);
+		$this->replace('{{ DummyFillAble }}', "'" .  implode("', '", $this->column['field']) . "'", $stubFile);
 	}
 
 	protected function getStub() {
 		return dirname(__DIR__, 1) . '/Stubs/Model.stub';
 	}
 
-	protected function replaceStub() {
-		$stubFile = $this->name . '.stub';
-		$namespace = empty($this->path) ? '' : '\\' . str_replace('/', '\\', $this->path);
-		$this->replace('{{ DummyNamespace }}', 'W7\App\Model\\Entity' . $namespace, $stubFile);
-		$this->replace('{{ DummyClass }}', $this->name, $stubFile);
+	protected function savePath() {
+		return 'app/Model/Entity/';
 	}
 
-	protected function savePath() {
-		return 'app/Model/Entity/' . $this->path;
+	private function getTableName($tableName, $connection = 'default') {
+		$tablePrefix = idb()->connection($connection)->getTablePrefix();
+		if (!Str::startsWith($tableName, $tablePrefix)) {
+			return $tablePrefix . $tableName;
+		} else {
+			return $tableName;
+		}
+	}
+
+	private function getTableColumn($tableName, $connection = 'default') {
+		$db = idb()->connection($connection);
+
+		$dirver = [
+			'mysql' => [
+				'query' => [
+					'sql' => "SELECT * FROM information_schema.columns WHERE table_schema = ? AND table_name = ?",
+					'params' => [$db->getDatabaseName(), $this->getTableName($tableName)]
+				],
+				'field' => [
+					'name' => 'COLUMN_NAME'
+				],
+				'primary' => [
+					'name' => 'COLUMN_KEY',
+					'value' => 'PRI'
+				]
+			],
+
+			'sqlite' => [
+				'query' => [
+					'sql' => "PRAGMA TABLE_INFO('" . $this->getTableName($tableName) . "')",
+					'params' => [],
+				],
+				'field' => [
+					'name' => 'name'
+				],
+				'primary' => [
+					'name' => 'pk',
+					'value' => '1'
+				]
+			]
+		];
+
+		$currentDriver = $dirver[$db->getConfig('driver')];
+		if (empty($currentDriver)) {
+			throw new RuntimeException('This database is not supported');
+		}
+		$result = $db->select($currentDriver['query']['sql'], $currentDriver['query']['params']);
+
+		if (empty($result)) {
+			throw new RuntimeException('The table is empty');
+		}
+
+		$column = [];
+		foreach ($result as $row) {
+			if ($row->{$currentDriver['primary']['name']} == $currentDriver['primary']['value']) {
+				$column['primaryKey'] = $row->{$currentDriver['field']['name']};
+			} else {
+				$column['field'][] = $row->{$currentDriver['field']['name']};
+			}
+		}
+		return $column;
+	}
+
+	private function parseTableNameToClassName($table) {
+		$name = [];
+
+		$tables = explode('_', $table);
+		foreach ($tables as $row) {
+			$name[] = ucfirst($row);
+		}
+
+		return implode("\\", $name);
 	}
 }
